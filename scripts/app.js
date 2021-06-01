0ac4832459310812bd1f422c417199bb193e0653
Bilal Keskin <bilalkeskin@Bilal-MacBook-Pro.local>
2021-06-01 20:54:23 +0300

    almost done

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   new Splide('#card-slider-2', {
     perPage: 1,
     type: 'loop',
-    height: 400,
+    height: 250,
     breakpoints: {
       600: {
         perPage: 1,

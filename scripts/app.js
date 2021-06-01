4d6c0e5817e231f52a88d5c6612a22df62e3ea6d
Bilal Keskin <bilalkeskin@Bilal-MacBook-Pro.local>
2021-06-01 19:49:32 +0300

    links provided

--- scripts/app.js
+++ scripts/app.js
@@ -10,3 +10,58 @@ document.addEventListener('DOMContentLoaded', function () {
     },
   }).mount();
 });
+
+document.addEventListener('submit', (e) => {
+  const url = 'https://cuengine.orgzit.com/api/1/record/';
+  const form = document.getElementById('form');
+  const formName = document.getElementById('name').value;
+  const formPhone = document.getElementById('phone').value;
+  const formMessage = document.getElementById('message').value;
+
+  const formData = {
+    dataform: '/api/1/dataform/pbynssdk2f/',
+    use_field_slug: true,
+    dataform_id: 'pbynssdk2f',
+    fields: {
+      name: formName,
+
+      phone: formPhone,
+
+      message: formMessage,
+
+      source: 'cuengineturkey.com',
+    },
+  };
+
+  async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'ApiKey bilalkeskin_94:523dacb2edb8d851cd3849f2c5b19b5ad6d20188',
+      },
+      body: JSON.stringify(data),
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+  postData(url, formData)
+    .then((data) => {
+      alert(
+        'You have succesfully send your information to us! Our team will contact you shortly.'
+      );
+      console.log(data);
+      return form.reset();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  e.preventDefault();
+});
